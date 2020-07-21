32d93881c4ddb567704497e684aeebd091a01064
Sahil Gunake <sahilgunake@gmail.com>
2020-07-21 21:55:47 +0530

    First commit

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float _speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if(transform.position.y >= 6f)
+        {
+            Destroy(this.gameObject, 5f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    private float _speedMultiplier = 2.0f;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private GameObject _tripleShotPrefab;
+
+    [SerializeField]
+    private GameObject _shieldVisualizer;
+
+    [SerializeField]
+    private GameObject _leftEngine, _rightEngine;
+
+    [SerializeField]
+    private float _fireRate = 0.15f;
+
+    private float _canFire = -1f;
+
+    [SerializeField]
+    private int _lives = 3;
+
+    private SpawnManager _spawnManager;
+
+    private UIManager _uiManager;
+
+    [SerializeField]
+    private int _score;
+
+    [SerializeField]
+    private AudioClip _laserSoundClip;
+
+ 
+    private AudioSource _audioSource;
+
+
+    
+
+    private bool _IsTripleShotActive = false;
+    private bool _IsSpeedBoostActive = false;
+    private bool _IsShieldActive = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0,0,0);
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        _audioSource = GetComponent<AudioSource>();
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL.");
+        }
+
+        if (_uiManager == null)
+        {
+            Debug.LogError("The UI Manager is NULL.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Calculate_Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire) 
+        {
+            FireLaser();
+        }
+    }
+
+    void Calculate_Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        // Movement of player
+
+            transform.Translate(direction * _speed * Time.deltaTime);
+
+        // Player constraints
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -4.86f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.86f, 0);
+        }
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+
+        if(_IsTripleShotActive == true)
+        {
+            Instantiate(_tripleShotPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+        }
+
+        _audioSource.Play();
+    }
+
+    public void Damage()
+    {
+        if(_IsShieldActive == true)
+        {
+            _IsShieldActive = false;
+            _shieldVisualizer.SetActive(false);
+            return;
+        }
+
+        _lives--;
+
+        if(_lives == 2)
+        {
+            _leftEngine.SetActive(true);
+        }
+        else if(_lives == 1)
+        {
+            _rightEngine.SetActive(true);
+        }
+
+        _uiManager.UpdateLives(_lives);
+
+        if (_lives < 1)
+        {
+            _spawnManager.OnPlayerDeath();
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    public void TripleShotActive()
+    {
+        _IsTripleShotActive = true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+    }
+
+    IEnumerator TripleShotPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _IsTripleShotActive = false;
+    }
+
+    public void SpeedBoostActive()
+    {
+        _IsSpeedBoostActive = true;
+        _speed *= _speedMultiplier;
+        StartCoroutine(SpeedBoostPowerDownRoutine());
+    }
+
+    IEnumerator SpeedBoostPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        _IsSpeedBoostActive = false;
+        _speed /= _speedMultiplier;
+    }
+
+    public void ShieldActive()
+    {
+        _IsShieldActive = true;
+        _shieldVisualizer.SetActive(true);
+        
+    }
+
+    public void AddScore(int points)
+    {
+        _score += points;
+        _uiManager.UpdateScore(_score);
+    }
+}
